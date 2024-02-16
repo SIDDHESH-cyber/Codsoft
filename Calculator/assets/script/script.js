a2221328f32f89fdf5c0b2523a303df2d39f90f7
Siddhesh Patel <70743244+SIDDHESH-cyber@users.noreply.github.com>
2024-02-17 00:45:53 +0530

    Create script.js

---
+++ Calculator/assets/script/script.js
@@ -0,0 +1,30 @@
+const input = document.getElementById('inputtext');
+const buttons = document.querySelectorAll('button');
+
+function calculate(expression) {
+    try {
+        return new Function('return ' + expression)();
+    } catch (error) {
+        return console.log(error);
+    }
+}
+
+
+function operation(buttonValue) {
+    if (buttonValue === 'C') {
+        input.value = '';
+    } else if (buttonValue === 'DEL') {
+        input.value = input.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+        input.value = calculate(input.value);
+    } else {
+        input.value += buttonValue;
+    }
+}
+
+buttons.forEach(button => {
+    let buttonValue = button.innerText;
+    button.addEventListener('click', function () {
+        operation(buttonValue);
+    });
+});
